ae299d3287fa76b53f2fe610439dd69780c6904a
feelings675 <feelings675@gmail.com>
2020-08-13 19:26:13 -0700

    hehehe

---
+++ src/BaiTap1.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+public class BaiTap1 {
+	public static void luuSoNhap (Scanner scan,LinkedList<Integer> listSo) {
+		System.out.println("Mời bạn nhập số muốn lưu vào list");
+		int i = scan.nextInt(); 
+		listSo.add(i);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int soNhap;
+		Scanner scan = new Scanner(System.in);
+		LinkedList<Integer> listSo = new LinkedList <Integer>();
+		
+		luuSoNhap(scan, listSo);
+	}
+}hahahaha A Thang Dep Trai vl va ngau vl ra 
+}hahahaha A Thang Dep Trai vl 
+Dep trai nhat the gioi
+hahahahahahahahahaha
+Co tinh de conflict ne ku 
